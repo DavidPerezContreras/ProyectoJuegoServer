68c852bc865ff23af32f81de1448ca603ced8a62
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-06-06 22:01:53 +0200

    Server2 refactoring.......

--- public/html/js/models.js
+++ public/html/js/models.js
@@ -1,4 +1,4 @@
-class Player{
+class Player {
     player;
     username;
     hearts;
@@ -7,40 +7,68 @@ class Player{
     x;
     y;
     xVel;
-    movingLeft=false;
-    movingRight=false;
-    acceleration=1000;
-    decceleration=1000;
+    movingLeft = false;
+    movingRight = false;
+    acceleration = 1000;
+    decceleration = 1000;
 
     //keyboard
-    pressedKeys={};
+    pressedKeys = {};
 
-    constructor(player, username){
-        this.player=player;
-        this.username=username;
-        this.x=300/2-(16/2);
-        this.y=150-16;
-        this.hearts=3;
+    constructor(player, username) {
+        this.player = player;
+        this.username = username;
+        this.x = 300 / 2 - (16 / 2);
+        this.y = 150 - 16;
+        this.hearts = 3;
     }
 }
 
-class Bullet{
+class Bullet {
     x;
     y;
 
-    constructor(x,y){
-        this.x=x;
-        this,y=y
+    constructor(x, y) {
+        this.x = x;
+        this, y = y
     }
 }
 
-class Room{
+//bullets = bullets.filter(obj => obj.x >= 0);
+
+
+//La idea es:
+//Mirar si hay salas con hueco.
+
+//Sino. Creamos la sala y luego llamamos addPlayer(player)
+class Room {
     player1;
     player2;
+    socket;
+    bullets;
 
-    bullets=[];
+    constructor() {
+        this.player1 = null;
+        this.player2 = null;
+        this.socket = null;
+        bullets = [];
+    }
+
+    addPlayer(player) {
+        if (!this.player1) {
+            this.player1 = player;
+            return true;
+        } else if (!this.player2) {
+            this.player2 = player;
+            return true;
+        } else {
+            return false;
+        }
+
+        player.room = this; // Assign the room object to the player
+        player.socket = this.socket; // Share the same socket among players
+    }
 }
-
 
 module.exports = {
     Player,
