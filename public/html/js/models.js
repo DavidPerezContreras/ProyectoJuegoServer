a9d103365bd7cb26e2323606ffa7e6ceab9b22ee
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-06-06 22:06:11 +0200

    bullet model typo

--- public/html/js/models.js
+++ public/html/js/models.js
@@ -2,8 +2,8 @@ class Player {
     player;
     username;
     hearts;
-
-    //movement
+  
+    // movement
     x;
     y;
     xVel;
@@ -11,29 +11,29 @@ class Player {
     movingRight = false;
     acceleration = 1000;
     decceleration = 1000;
-
-    //keyboard
+  
+    // keyboard
     pressedKeys = {};
-
+  
     constructor(player, username) {
-        this.player = player;
-        this.username = username;
-        this.x = 300 / 2 - (16 / 2);
-        this.y = 150 - 16;
-        this.hearts = 3;
+      this.player = player;
+      this.username = username;
+      this.x = 300 / 2 - (16 / 2);
+      this.y = 150 - 16;
+      this.hearts = 3;
     }
-}
-
-class Bullet {
+  }
+  
+  class Bullet {
     x;
     y;
-
+  
     constructor(x, y) {
-        this.x = x;
-        this, y = y
+      this.x = x;
+      this.y = y;
     }
-}
-
+  }
+  
 //bullets = bullets.filter(obj => obj.x >= 0);
 
 
@@ -41,37 +41,38 @@ class Bullet {
 //Mirar si hay salas con hueco.
 
 //Sino. Creamos la sala y luego llamamos addPlayer(player)
-class Room {
+
+  class Room {
     player1;
     player2;
     socket;
     bullets;
-
+  
     constructor() {
-        this.player1 = null;
-        this.player2 = null;
-        this.socket = null;
-        bullets = [];
+      this.player1 = null;
+      this.player2 = null;
+      this.socket = null;
+      bullets = []; // Array to store bullets
     }
-
+  
     addPlayer(player) {
-        if (!this.player1) {
-            this.player1 = player;
-            return true;
-        } else if (!this.player2) {
-            this.player2 = player;
-            return true;
-        } else {
-            return false;
-        }
-
-        player.room = this; // Assign the room object to the player
-        player.socket = this.socket; // Share the same socket among players
+      if (!this.player1) {
+        this.player1 = player;
+        return true;
+      } else if (!this.player2) {
+        this.player2 = player;
+        return true;
+      } else {
+        return false; // Room is already full
+      }
+  
+      player.room = this; // Assign the room object to the player
+      player.socket = this.socket; // Share the same socket among players
     }
-}
-
-module.exports = {
+  }
+  
+  module.exports = {
     Player,
     Bullet,
     Room
-}
+  };
