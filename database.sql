03fb5abc9771369548a260d805611e15b745b0aa
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-06-10 04:06:22 +0200

    database.sql

---
+++ database.sql
@@ -0,0 +1,62 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema proyectojuego
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema proyectojuego
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `proyectojuego` DEFAULT CHARACTER SET utf8 ;
+USE `proyectojuego` ;
+
+-- -----------------------------------------------------
+-- Table `proyectojuego`.`scores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `proyectojuego`.`scores` (
+  `username` VARCHAR(20) NOT NULL,
+  `wins` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`username`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `proyectojuego`.`sessions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `proyectojuego`.`sessions` (
+  `session_id` VARCHAR(255) NOT NULL,
+  `expires` DATETIME NOT NULL,
+  `data` TEXT NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
+  `username` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`session_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `proyectojuego`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `proyectojuego`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) ,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
