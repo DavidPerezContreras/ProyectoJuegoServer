d27a6566e3727b8b8f6fc2556758833e923c0970
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-06-14 19:28:56 +0200

    FINISHED ✔ login fix + server.js

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
+const { express, session, sessionStore, sharedSession, sessionMiddleware, dbConfig, app, httpServer, io, path, cors } = require("./js/config");
 
-//Initialize Configuration
-const { express, session, sessionStore, app, httpServer, socket, path, cors } = require("./js/config");
-
-const routes = require("./js/routes");
-
-const {mysql,MySQLStore} = require("./js/mysql");
-
-const {Player, Bullet, Room} = require("./public/html/js/models");
-const {initializeKeyboard, pressedKeys} = require("./js/keyboard");//TODO: Not session depeendant
-initializeKeyboard();
-
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-app.use("/", routes);
+const { mysql, MySQLStore } = require("./js/mysql");
+const { Player, Bullet, Room } = require("./public/html/js/models");
 
 
 
-httpServer.listen(3000, () => {
-    console.log("Server is running on port 3000");
+httpServer.listen(80, () => {
+  console.log("Server is running on port 3000");
 });
 
 
@@ -29,51 +17,162 @@ httpServer.listen(3000, () => {
 
 
 
-//var rooms=[];
-
-//TODO: Si encuentra alguna partida que haya hueco, mete el jugador en esa partida
 
 
+const SKIN_WIDTH = 16;
+const SKIN_HEIGHT = 16;
+const BULLET_RADIUS = 4;
 
-//var player1=new Player(1,"david",3 );
-//rooms[0].player1=player1;
-
+var pressedKeys = {};
 
 
 
+var rooms = [];
 
-  
+io.on('connection', (socket) => {
+  socket.on("joinRoom", (data) => {
+    var player = new Player(data.username);
+    console.log(player.username + " requested to join a Room.");
+
+    function createNewRoom(player) {
+      var room = new Room(rooms.length);
+      room.player1 = player;
+      rooms.push(room);
+      socket.emit("roomJoined", room);
+      console.log(player.username + " joining room " + room.id);
+      socket.join(room.id);
+      console.log(`Client joined channel: ${room.id}`);
+      io.to(room.id).emit('roomState', room);
+    }
+
+    function joinExistingRoom(player, room) {
+      socket.emit("roomJoined", room);
+      console.log(player.username + " joining room " + room.id);
+      socket.join(room.id);
+      console.log(`Client joined channel: ${room.id}`);
+      io.to(room.id).emit('roomState', room);
+    }
+
+    function freeSlot(player) {
+      // Iterate through rooms to find and free the player's slot
+      for (var i = 0; i < rooms.length; i++) {
+        var room = rooms[i];
+
+        if (room.player1 && room.player1.username === player.username) {
+          room.player1 = null; // Free player1 slot
+          console.log(player.username + "'s slot in room " + room.id + " is now free.");
+        }
+
+        if (room.player2 && room.player2.username === player.username) {
+          room.player2 = null; // Free player2 slot
+          console.log(player.username + "'s slot in room " + room.id + " is now free.");
+        }
+      }
+    }
+
+    // Check if there are any rooms
+    if (rooms.length === 0) {
+      createNewRoom(player); // Create a new room for the player
+    } else {
+      var roomFound = false;
+
+      // Iterate through existing rooms
+      for (var i = 0; i < rooms.length; i++) {
+        var room = rooms[i];
+
+        // Check if any player in the room has the same username
+        if (room.player1 && room.player1.username === player.username) {
+          continue; // Skip this room, as player1 has the same username
+        }
+
+        if (room.player2 && room.player2.username === player.username) {
+          continue; // Skip this room, as player2 has the same username
+        }
+
+        // If there is an available slot, join the room
+        if (!room.player1) {
+          room.player1 = player;
+        } else if (!room.player2) {
+          room.player2 = player;
+        }
+
+        // If a slot was assigned, join the room
+        if (room.player1 || room.player2) {
+          joinExistingRoom(player, room);
+          roomFound = true;
+          break;
+        }
+      }
+
+      // If no room was found, create a new room
+      if (!roomFound) {
+        createNewRoom(player);
+      }
+    }
+
+    console.log(rooms);
+
+    // When the socket disconnects, free up the player's slot
+    socket.on("disconnect", () => {
+      freeSlot(player);
+      console.log(player.username + " has disconnected.");
+      console.log(rooms);
+    });
+  });
 
 
 
+  socket.on("onkeypress", (data) => {
+    if (!pressedKeys[data.key]) {
+      //console.log("onkeypress    " + data.key + "    by  " + data.username + " to Room " + data.roomId);
+
+      var room = rooms[data.roomId];
+      var player;
+
+      if (room.player1.username === data.username) {
+        player = room.player1;
+      } else if (room.player2.username === data.username) {
+        player = room.player2;
+
+      }
+
+      player.pressedKeys[data.key] = true;
+      //console.log(player.pressedKeys);
+    }
+  });
+
+  socket.on("onkeyup", (data) => {
+    //console.log("onkeyup    " + data.key + "    by  " + data.username + " to Room " + data.roomId);
+
+    var room = rooms[data.roomId];
+    var player;
+
+    if (room.player1.username === data.username) {
+      player = room.player1;
+
+    } else if (room.player2.username === data.username) {
+      player = room.player2;
+
+    }
 
 
 
+    player.pressedKeys[data.key] = false;
+    //console.log(player.pressedKeys);
 
 
+  });
 
-// Initialize position variables
-let x = 0;
-
-// Initialize velocity variables
-let xVel = 0;
-const acceleration = 1000; // adjust as needed
-const deceleration = 1000; // adjust as needed
-
-let movingLeft = false;
-let movingRight = false;
-
-
-
+});
 
 
 
 const hrtimeMs = function () {
-    let time = process.hrtime()
-    return time[0] * 1000 + time[1] / 1000000
+  let time = process.hrtime()
+  return time[0] * 1000 + time[1] / 1000000
 }
 
-const TICK_RATE = 20;
+const TICK_RATE = 128;
 let previous = hrtimeMs()
 let tickLengthMs = 1000 / TICK_RATE
 //let tick; //tick count unused?
@@ -87,44 +186,17 @@ let delta;
 //bullet
 /////////////////////////////////////////////////////////////////////
 
-let bullets = []; // Array to store all bullets
-let bulletId = 0; // Unique ID for each bullet
 
-// Function to spawn a new bullet at the player's position
-const spawnBullet = () => {
-  const bullet = {
-    id: bulletId++,
-    x: 0,
-    y:150,
-    velocity: {
-      x: 0,
-      y: -66// Set the velocity to move upwards
-    }
-  };
-  bullets.push(bullet);
-};
 
-// Function to update the position of all bullets
-const updateBullets = () => {
-  bullets.forEach((bullet) => {
-    bullet.x += bullet.velocity.x;
-    bullet.y += bullet.velocity.y*delta ;
-  });
-};
-
-// Emit the current position of all bullets to all clients
-const emitBullets = async () => {
-  socket.emit('bulletsUpdated', bullets);
-};
 
 
 
 
 //For each active Room, 
-    //Calculate player 1 and player 2 state
-    //Calculate Room bullets positions
+//Calculate player 1 and player 2 state
+//Calculate Room bullets positions
 
-    //Emit Room objects every tick
+//Emit Room objects every tick
 
 
 
@@ -136,84 +208,62 @@ const emitBullets = async () => {
 
 
 const loop = () => {
-    setTimeout(loop, tickLengthMs)
-    let now = hrtimeMs()
-    delta = (now - previous) / 1000
+  setTimeout(loop, tickLengthMs);
+  let now = hrtimeMs();
+  delta = (now - previous) / 1000;
 
-    tickrate = 1 / delta;
-    //console.log("tickrate= " + tickrate);
-    ///////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
+  tickrate = 1 / delta;
+  //console.log("tickrate= " + tickrate);
+  ///////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////
 
 
-    // Handle key presses
-    if (pressedKeys["a"] && !pressedKeys["d"]) {
-        movingLeft = true;
-        movingRight = false;
-    } else if (pressedKeys["d"] && !pressedKeys["a"]) {
-        movingLeft = false;
-        movingRight = true;
-    } else {
-        movingLeft = false;
-        movingRight = false;
+
+  for (let i = 0; i < rooms.length; i++) {
+    let room = rooms[i];
+    if (room.player1) {
+      //player1 x
+      room.player1.updatePlayerData(delta)
+
+      //player1 room bullets
+      if (room.player1.pressedKeys[" "]) {
+        room.spawnBullet(room.player1);
+
+        room.player1.pressedKeys[" "] = false;
+      }
     }
 
-    // Apply acceleration and deceleration
-    if (movingLeft) {
-        xVel = Math.max(xVel - acceleration * delta, -100);
-    } else if (movingRight) {
-        xVel = Math.min(xVel + acceleration * delta, 100);
-    } else if (xVel > 0) {
-        xVel = Math.max(xVel - deceleration * delta, 0);
-    } else if (xVel < 0) {
-        xVel = Math.min(xVel + deceleration * delta, 0);
+    if (room.player2) {
+      //player2 x
+      room.player2.updatePlayerData(delta)
+
+      //spawn player2 room bullets
+      if (room.player2.pressedKeys[" "]) {
+        room.spawnBullet(room.player2);
+
+        room.player2.pressedKeys[" "] = false;
+      }
+
+
     }
 
+    room.updateRoomBullets(delta);
 
-    // Update position based on velocity
+    room.checkBulletHits();
 
-    x += xVel * delta;
-
-    if (x < 0) {
-        x = 0;
-        xVel = 0;
-    } else {
-        if (x > 280) {
-            x = 280;
-            xVel = 0;
-        }
-    }
-
-
-    //console.log("emit X = " + x);
+  }
 
 
 
-    var emit = async ()=> {
-        socket.emit("message", { x: x });
-    }
-    emit.call();
+  for (let i = 0; i < rooms.length; i++) {
+    const value = rooms[i];
+    io.to(i).emit('roomState', value);
+  }
 
 
 
-
-
-    if(pressedKeys[" "]){
-        spawnBullet();
-        pressedKeys[" "]=false;
-    }
-
-
-
-    updateBullets();
-    emitBullets();
-
-
-    
-    ///////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-    previous = now;
-    //tick++;
+  /////////////////////////////////////////////////////////////////////////////
+  previous = now;
 }
 
 
